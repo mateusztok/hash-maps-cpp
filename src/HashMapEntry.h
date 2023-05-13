e27c17699d4aa78daa1b4db21772897658d3f6c9
raczu <Raczuekk@gmail.com>
2023-05-13 22:48:57 +0200

    refactor: change HashNode implementation

---
+++ src/HashMapEntry.h
@@ -0,0 +1,31 @@
+#pragma once
+
+template <typename K, typename V>
+class HashMapEntry {
+protected:
+    K _key;
+    V _value;
+
+public:
+    HashMapEntry(const K &key, const V &value);
+    ~HashMapEntry();
+
+    K getKey();
+    V getValue();
+    void setValue(const V &value);
+};
+
+template <typename K, typename V>
+HashMapEntry<K, V>::HashMapEntry(const K &key, const V &value) : _key(key), _value(value) {}
+
+template <typename K, typename V>
+HashMapEntry<K, V>::~HashMapEntry() = default;
+
+template <typename K, typename V>
+K HashMapEntry<K, V>::getKey() { return _key; }
+
+template <typename K, typename V>
+V HashMapEntry<K, V>::getValue() { return _value; }
+
+template <typename K, typename V>
+void HashMapEntry<K, V>::setValue(const V &value) { _value = value; }
