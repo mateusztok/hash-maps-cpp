7ff421f6f9e4090e290b3960082607bbda89c323
raczu <Raczuekk@gmail.com>
2023-06-14 17:16:12 +0200

    chore: add benchmarks

---
+++ benchmark/benchmark.cpp
@@ -0,0 +1,247 @@
+#include <hashmaps/HashMapLL.h>
+#include <hashmaps/HashMapDH.h>
+#include <hashmaps/HashMapRH.h>
+
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <ctime>
+#include <sstream>
+
+std::vector<std::string> getElements(const std::string& s) {
+    std::vector<std::string> row;
+    std::string element;
+
+    bool inQuotes = false;
+    for (auto &c: s) {
+        if (c == '"') inQuotes = !inQuotes;
+        else if (c == ',' && !inQuotes) {
+            element.erase(std::remove(element.begin(), element.end(), '\"'), element.end());
+            row.push_back(element); element.clear();
+        } else element += c;
+    }
+
+    element.erase(std::remove(element.begin(), element.end(), '\"'), element.end());
+    row.push_back(element);
+    return row;
+}
+
+std::vector<std::vector<std::string>> getData(const std::string& filename) {
+    std::vector<std::vector<std::string>> data;
+    std::fstream file;
+    file.open(filename, std::fstream::in);
+    std::string line;
+
+    if (file.is_open()) {
+        std::getline(file, line);
+        while(!file.eof()) {
+            std::getline(file, line);
+            data.push_back(getElements(line));
+        }
+        file.close();
+    }
+    return data;
+}
+
+size_t nearestPowerOf2(size_t number) {
+    if (number < 1) return number;
+    auto power = static_cast<size_t>(std::pow(2, std::floor(std::log2(number))));
+    if (number - power <= power / 2) return power;
+    else return power * 2;
+}
+
+std::string convertToString(char *buffer) {
+    std::string s(buffer);
+    return s;
+}
+
+std::vector<std::string> analyse(const std::vector<std::vector<std::string>>& data) {
+    std::vector<int> values = {50, 100, 250, 500, 1000, 5000, 10000, 15000, 30000, 50000, 75000, 100000, 150000};
+    std::vector<float> loadFactors = {0.75f, 0.80f, 0.90f, 0.95f, 0.99f};
+
+    HashMapLL<std::string, float> *hashMapLL;
+    HashMapDH<std::string, float> *hashMapDH;
+    HashMapRH<std::string, float> *hashMapRH;
+
+    std::vector<std::string> results;
+    results.emplace_back("HashMap,\"Number of elements\",Load factor,Operation,Time");
+    char buffer[100];
+
+    for (float loadFactor : loadFactors) {
+        for (int value : values) {
+            auto hashMapSize = static_cast<size_t>(std::floor(value / loadFactor));
+            hashMapLL = new HashMapLL<std::string, float>(nearestPowerOf2(hashMapSize), loadFactor);
+            hashMapDH = new HashMapDH<std::string, float>(hashMapSize, loadFactor);
+            hashMapRH = new HashMapRH<std::string, float>(nearestPowerOf2(hashMapSize), loadFactor);
+
+            // Adding elements to hash maps and benchmarking performance
+            auto start = std::chrono::high_resolution_clock::now();
+            for (size_t e = 0; e < value; e++) {
+                hashMapLL->put(data[e][0], std::stof(data[e][1]));
+            }
+            auto stop = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+            std::sprintf(buffer,
+                         "%s,%d,%.2f,%s,%lld",
+                         "LL",
+                         value,
+                         loadFactor,
+                         "put",
+                         duration.count());
+            results.push_back(convertToString(buffer));
+
+            start = std::chrono::high_resolution_clock::now();
+            for (size_t e = 0; e < value; e++) {
+                hashMapDH->put(data[e][0], std::stof(data[e][1]));
+            }
+            stop = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+
+            start = std::chrono::high_resolution_clock::now();
+            for (size_t e = 0; e < value; e++) {
+                hashMapRH->put(data[e][0], std::stof(data[e][1]));
+            }
+            stop = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+            std::sprintf(buffer,
+                         "%s,%d,%.2f,%s,%lld",
+                         "RH",
+                         value,
+                         loadFactor,
+                         "put",
+                         duration.count());
+            results.push_back(convertToString(buffer));
+
+
+            // Coping data for shuffling and further benchmarks
+            std::vector<std::vector<std::string>> copiedData(data.begin(), data.begin() + value);
+            std::random_device rd;
+            std::mt19937 rng(rd());
+            std::shuffle(copiedData.begin(), copiedData.end(), rng);
+
+            // Looking up for elements in hash maps and benchmarking performance
+            start = std::chrono::high_resolution_clock::now();
+            for (size_t e = 0; e < value; e++) {
+                hashMapLL->containsKey(copiedData[e][0]);
+            }
+            stop = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+            std::sprintf(buffer,
+                         "%s,%d,%.2f,%s,%lld",
+                         "LL",
+                         value,
+                         loadFactor,
+                         "containsKey",
+                         duration.count());
+            results.push_back(convertToString(buffer));
+
+            start = std::chrono::high_resolution_clock::now();
+            for (size_t e = 0; e < value; e++) {
+                hashMapRH->containsKey(copiedData[e][0]);
+            }
+            stop = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+            std::sprintf(buffer,
+                         "%s,%d,%.2f,%s,%lld",
+                         "RH",
+                         value,
+                         loadFactor,
+                         "containsKey",
+                         duration.count());
+            results.push_back(convertToString(buffer));
+
+            // Intentionally looking for element that does not exist in hash maps and benchmarking performance
+            start = std::chrono::high_resolution_clock::now();
+            hashMapLL->containsKey("im-not-existing");
+            stop = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+            std::sprintf(buffer,
+                         "%s,%d,%.2f,%s,%lld",
+                         "LL",
+                         value,
+                         loadFactor,
+                         "containsKeyFailed",
+                         duration.count());
+            results.push_back(convertToString(buffer));
+
+            start = std::chrono::high_resolution_clock::now();
+            hashMapLL->containsKey("im-not-existing");
+            stop = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+            std::sprintf(buffer,
+                         "%s,%d,%.2f,%s,%lld",
+                         "RH",
+                         value,
+                         loadFactor,
+                         "containsKeyFailed",
+                         duration.count());
+            results.push_back(convertToString(buffer));
+
+            // Removing elements from hash maps and benchmarking performance
+            for (size_t e = 0; e < value; e++) {
+                hashMapLL->remove(copiedData[e][0]);
+            }
+            stop = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+            std::sprintf(buffer,
+                         "%s,%d,%.2f,%s,%lld",
+                         "LL",
+                         value,
+                         loadFactor,
+                         "remove",
+                         duration.count());
+            results.push_back(convertToString(buffer));
+
+            start = std::chrono::high_resolution_clock::now();
+            for (size_t e = 0; e < value; e++) {
+                hashMapRH->remove(copiedData[e][0]);
+            }
+            stop = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+            std::sprintf(buffer,
+                         "%s,%d,%.2f,%s,%lld",
+                         "RH",
+                         value,
+                         loadFactor,
+                         "remove",
+                         duration.count());
+            results.push_back(convertToString(buffer));
+        }
+    }
+
+    return results;
+}
+
+std::string writeToCSVFile(const std::vector<std::string>& results) {
+    auto currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::tm* timeInfo = std::localtime(&currentTime);
+    std::ostringstream oss;
+    oss << std::put_time(timeInfo, "%d-%m-%Y_%H:%M:%S");
+    char buffer[100];
+    std::sprintf(buffer, "results-%s.csv", oss.str().c_str());
+
+    std::fstream file;
+    file.open(convertToString(buffer), std::fstream::out | std::fstream::trunc);
+    if (file.is_open()) {
+        for (size_t i = 0; i < results.size(); i++) {
+            if (i == results.size() - 1) file << results[i];
+            else file << results[i] << "\n";
+        }
+        file.close();
+        return "Successfully saved results to file!";
+    }
+
+    return "There was a problem with saving results to file";
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) throw std::invalid_argument("Benchmark requires the path of file from which it reads data");
+    std::vector args(argv + 1, argv + argc);
+
+    auto results = analyse(getData(args[0]));
+    std::cout << writeToCSVFile(results) << "\n";
+
+    return 0;
+}
---
+++ benchmark/utils/ploter.py
@@ -0,0 +1,136 @@
+import argparse
+import csv
+import matplotlib.pyplot as plt
+
+from os import listdir, mkdir
+from os.path import exists, isdir, isfile, join
+from dataclasses import dataclass
+from enum import Enum
+from copy import deepcopy
+
+
+class Operations(Enum):
+    PUT = 'PUT'
+    CONTAINS_KEY = 'LOOKUP'
+    REMOVE = 'REMOVE'
+    CONTAINS_KEY_FAILED = 'FAILED LOOKUP'
+
+
+CONVERTER = {
+    'put': Operations.PUT,
+    'containsKey': Operations.CONTAINS_KEY,
+    'remove': Operations.REMOVE,
+    'containsKeyFailed': Operations.CONTAINS_KEY_FAILED
+}
+
+
+@dataclass
+class HashMapResults:
+    name: str
+    ranges: list[int]
+    results: dict[Operations, list[list[int]]]
+
+    def get_average(self, operation: Operations) -> list[int]:
+        return [sum(elements) / len(elements) for elements in zip(*self.results[operation])]
+
+    def get_operations(self) -> list[Operations]:
+        return [k for k, v in self.results.items()]
+
+
+def get_results(files: list[str]) -> dict[str, list[HashMapResults]]:
+    ranges = list()
+    data = dict()
+
+    operations = {
+        Operations.PUT: [[] for _ in range(len(files))],
+        Operations.CONTAINS_KEY: [[] for _ in range(len(files))],
+        Operations.REMOVE: [[] for _ in range(len(files))],
+        Operations.CONTAINS_KEY_FAILED: [[] for _ in range(len(files))]
+    }
+
+    for idx, filename in enumerate(files):
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            next(reader, None)
+            for row in reader:
+                if row[1] not in ranges:
+                    ranges.append(row[1])
+
+                if row[2] not in data:
+                    data[row[2]] = [HashMapResults(name, list(), deepcopy(operations)) for name in ('LL', 'DH', 'RH')]
+
+                match row[0]:
+                    case 'LL':
+                        data[row[2]][0].results[CONVERTER[row[3]]][idx].append(int(row[4]))
+                    case 'DH':
+                        data[row[2]][1].results[CONVERTER[row[3]]][idx].append(int(row[4]))
+                    case 'RH':
+                        data[row[2]][2].results[CONVERTER[row[3]]][idx].append(int(row[4]))
+
+    for _, v in data.items():
+        for hm in v:
+            hm.ranges = ranges
+
+    return data
+
+
+def format_number(number: int) -> str:
+    if 1e3 <= number < 1e6:
+        return f"{int(number / 1e3)}K"
+    elif number >= 1e6:
+        return f"{int(number / 1e6)}M"
+    else:
+        return str(int(number))
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description='Arguments to configure a script')
+
+    parser.add_argument(
+        '--results',
+        type=str,
+        required=True,
+        help='specify a path to result files')
+
+    parser.add_argument(
+        '--save',
+        action='store_true',
+        help='specify if generated plots should be saved')
+
+    args = parser.parse_args()
+    if not exists(args.results) and not isdir(args.results):
+        raise FileNotFoundError('Cannot find specified files')
+
+    files = [join(args.results, r) for r in listdir(args.results) if isfile(join(args.results, r))]
+    results = get_results(files)
+
+    mkdir('./assets') if args.save and not isdir('./assets') else None
+
+    for lf, maps in results.items():
+        ll, dh, rh = maps
+        for operation in ll.get_operations():
+            x = [format_number(int(n)) for n in ll.ranges]
+            if operation == Operations.CONTAINS_KEY_FAILED:
+                plt.plot(x, ll.get_average(operation), label=ll.name, linestyle='-', marker='o', markersize=3.5)
+                plt.plot(x, dh.get_average(operation), label=dh.name, linestyle='-', marker='o', markersize=3.5)
+                plt.plot(x, rh.get_average(operation), label=rh.name, linestyle='-', marker='o', markersize=3.5)
+                plt.ylabel('Time [ns]')
+            else:
+                plt.plot(x, list(map(lambda ns: ns / 1000000, ll.get_average(operation))),
+                         label=ll.name, linestyle='-', marker='o', markersize=3.5)
+                plt.plot(x, list(map(lambda ns: ns / 1000000, dh.get_average(operation))),
+                         label=rh.name, linestyle='-', marker='o', markersize=3.5)
+                plt.plot(x, list(map(lambda ns: ns / 1000000, dh.get_average(operation))),
+                         label=rh.name, linestyle='-', marker='o', markersize=3.5)
+                plt.ylabel('Time [ms]')
+            plt.xticks(x)
+            plt.grid()
+            plt.legend()
+            plt.xlabel('Number of entries in hash map')
+            plt.title(f'{operation.value} (load factor {lf})', fontweight='bold')
+            plt.show()
+
+
+if __name__ == '__main__':
+    main()
